a0b537859d7c29534616b083aa9e77d2e9c53640
root <root@localhost.localdomain>
2017-09-21 05:17:38 -0400

    basic framework using docker-compose
    Signed-off-by: root <root@localhost.localdomain>

--- aurora/wsgi.py
+++ aurora/wsgi.py
@@ -1,12 +1,62 @@
 # wsgi.py
 
-from flask import Flask
+from flask import g, Flask, jsonify
+from flask_admin import Admin, BaseView, expose
+from flask_login import LoginManager, current_user
 
 def create_app():
   app = Flask(__name__)
   return app
 
 application = create_app()
+app = application
+
+login_manager = LoginManager(app)
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'index'
+
+admin = Admin(app)
+
+class MyView(BaseView):
+    #127.0.0.1:5000/admin/
+    @expose('/')
+    def index(self):
+        #return self.render('index.html')
+        return '<html>Hello, admin</html>'
+
+    def is_accessible(self):
+        return g.user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('auth.login', next=request.url))
+
+admin.add_view(MyView(name='Hello'))
+
+admin.add_view(MyView(name='Hello 1', endpoint='test1', category='Test'))
+admin.add_view(MyView(name='Hello 2', endpoint='test2', category='Test'))
+admin.add_view(MyView(name='Hello 3', endpoint='test3', category='Test'))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+@app.route('/api/v1/auth/login')
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return (jsonify({'username':'test'}), 201,
+            {'Location': url_for('index', id='id', _external=True)})
+
+@app.route('/api/v1/')
+def index():
+    return jsonify({'index':'ok'})
+
+
+@app.route('/api/v1/test')
+def test():
+    return jsonify({'test':'ok'})
 
 if __name__ == '__main__':
     application.run()
--- docker/start-server.sh
+++ docker/start-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-gunicorn -w 4 -b 127.0.0.0:8009 wsgi:application
+cd ../aurora
+gunicorn --reload -w 4 -b 0.0.0.0:8009 wsgi:application --error-logfile /var/log/gunicorn/gunicorn.log --log-level debug
